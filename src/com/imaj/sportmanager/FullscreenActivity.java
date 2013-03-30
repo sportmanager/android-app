707353cf8e7e8f964b08209d40c7937ef39306b4
Ivan Matyash <john.matyash@gmail.com>
2013-03-30 10:29:25 +0200

    Removed overrides

--- src/com/imaj/sportmanager/FullscreenActivity.java
+++ src/com/imaj/sportmanager/FullscreenActivity.java
@@ -65,7 +65,6 @@ public class FullscreenActivity extends Activity {
 					int mControlsHeight;
 					int mShortAnimTime;
 
-					@Override
 					@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
 					public void onVisibilityChange(boolean visible) {
 						if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
@@ -101,7 +100,6 @@ public class FullscreenActivity extends Activity {
 
 		// Set up the user interaction to manually show or hide the system UI.
 		contentView.setOnClickListener(new View.OnClickListener() {
-			@Override
 			public void onClick(View view) {
 				if (TOGGLE_ON_CLICK) {
 					mSystemUiHider.toggle();
@@ -134,7 +132,6 @@ public class FullscreenActivity extends Activity {
 	 * while interacting with activity UI.
 	 */
 	View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-		@Override
 		public boolean onTouch(View view, MotionEvent motionEvent) {
 			if (AUTO_HIDE) {
 				delayedHide(AUTO_HIDE_DELAY_MILLIS);
@@ -145,7 +142,6 @@ public class FullscreenActivity extends Activity {
 
 	Handler mHideHandler = new Handler();
 	Runnable mHideRunnable = new Runnable() {
-		@Override
 		public void run() {
 			mSystemUiHider.hide();
 		}
